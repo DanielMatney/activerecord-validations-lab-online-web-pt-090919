7ddb52190e6ff351230584ca30e80257c1723694
Daniel Matney <danieljmatney@gmail.com>
2020-03-03 19:23:26 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,14 @@
 class Post < ActiveRecord::Base
+<<<<<<< HEAD
   include ActiveModel::Validations
+=======
+>>>>>>> b80fd4fc7ab02a43893e14b21cb7a714a8f7ffb2
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction), 
   message: "%{value} must be Fiction or Non-Fiction" }
+<<<<<<< HEAD
   validate :non_clickbait
   
   def non_clickbait
@@ -16,3 +20,6 @@ class Post < ActiveRecord::Base
   
   
 end
+=======
+end
+>>>>>>> b80fd4fc7ab02a43893e14b21cb7a714a8f7ffb2
--- app/validators/post_validator.rb
+++ app/validators/post_validator.rb
@@ -1,7 +1,13 @@
 class PostValidator < ActiveModel::Validator
   def non_clickbait(record)
+<<<<<<< HEAD
     unless record.title.match?("Won't Believe") || record.title.match?("Secret") || record.title.match?("Top [number]") || record.title.match?("Guess")
       errors.add(:title, "Title not clickbaitey enough!")
+=======
+    unless record.title.match?("Won't Believe") || record.title.match?("Secret") || record.titl
+      validates record, inclusion: { in: %w("Won't Believe" "Secret" "Top [number]" "Guess"), 
+  message: "%{value} must be clickbaitey" }
+>>>>>>> b80fd4fc7ab02a43893e14b21cb7a714a8f7ffb2
     end
   end
 end
